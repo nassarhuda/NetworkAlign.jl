024dceef0d50efc343b520efe4393d9d24d4231a
Charlie <ccolley@purdue.edu>
2021-09-28 13:59:27 -0400

    updated types in netalign_setup

--- src/netalign_setup.jl
+++ src/netalign_setup.jl
@@ -1,23 +1,7 @@
 # % NETALIGN_SETUP Setup the data for the netalign codes from the problem data
 
-function netalign_setup(A::SparseMatrixCSC{Int64,Int64},B::SparseMatrixCSC{Int64,Int64},
-                        L::SparseMatrixCSC{Int64,Int64})
-
-undirected = issymmetric(A) && issymmetric(B)
-Se,Le,LeWeights = make_squares(A,B,L,undirected)
-
-li = Le[:,1]
-lj = Le[:,2]
-Se1 = Se[:,1]
-Se2 = Se[:,2]
-S = sparse(Se1,Se2,1,nnz(L),nnz(L))
-
-return (S,LeWeights,li,lj)
-
-end
-
-function netalign_setup(A::SparseMatrixCSC{Int64,Int64},B::SparseMatrixCSC{Int64,Int64},
-                        L::SparseMatrixCSC{Float64,Int64})
+function netalign_setup(A::SparseMatrixCSC{T,Int64},B::SparseMatrixCSC{T,Int64},
+                        L::SparseMatrixCSC{Q,Int64}) where {T,Q}
 
 undirected = issymmetric(A) && issymmetric(B)
 Se,Le,LeWeights = make_squares(A,B,L,undirected)
